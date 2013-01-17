beee6ae3fe4ecd22305c9f557c36dff44529f3f1
JA44 <jeanalain.re@gmail.com>
2013-01-17 13:40:48 +0100

    ChessClock : 	- ajout 0 si les heures, minutes, secondes < 10

--- utils.js
+++ utils.js
@@ -10,8 +10,21 @@ utils.date.getFormat = function(time, format){
 	    var hours = Math.round( time / 3600 );
 	    var minuts = Math.floor(  ( time % 3600 ) / 60 );
 	    var seconds = ( time % 3600 ) % 60 ;
+	    
+	    hours = utils.date.addZero(hours);
+	    minuts = utils.date.addZero(minuts);
+	    seconds = utils.date.addZero(seconds);
+
 	    return hours + ':' + minuts + ':' + seconds;
 	    break
+    }
+    return buffer;
+}
+//Format 1 en 01
+utils.date.addZero = function(val){
+    var buffer = val;
+    if(val < 10){
+	buffer = '0' + val;
     }
     return buffer;
 }
