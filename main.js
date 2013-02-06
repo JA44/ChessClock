3c600d7d954d6d20aa1a079d5842073e29587b89
JA44 <jeanalain.re@gmail.com>
2013-02-06 11:10:54 +0100

    ChessClock :  - Gestion play/pause  - Correction interface

--- main.js
+++ main.js
@@ -36,13 +36,23 @@ $(function(){
 	    var that = arguments.length != 0 ? arguments[0] : this;
 	    that.set({time_remaining : that.get('time_remaining') - 1 });
 	},
+	play: function(){
+	    this.set({currentIntervalId: setInterval(this.decrementTime, 1000, this)});
+	},
+	stop: function(){
+	    clearInterval( this.get('currentIntervalId') );
+	    this.set({currentIntervalId: null});
+	},
+	isPlaying: function(){
+	    return this.get('currentIntervalId') !== null;
+	},
 	setCurrent: function(current){
 	    if(current){
 		this.set({current: true});
-		this.set({currentIntervalId: setInterval(this.decrementTime, 1000, this)});
+		this.play();
 	    }else{
 		this.set({current: false});
-		clearInterval( this.get('currentIntervalId') );
+		this.stop();
 	    }
 	}	
    });
@@ -54,11 +64,10 @@ $(function(){
         el : $('body'),
 	//events: events,
 	events: {
-	   'click #switch' :  'switchPlayer',
-	   'click #config' :  'params',
-	   'click #pause'  :  'pause',
-	   'click #play'  :  'play',
-	   'click #stop'  :  'stop'
+	   'click #switch'	:  'switchPlayer',
+	   'click #config'	:  'params',
+	   'click #play-pause'  :  'playOrPause',
+	   'click #stop'	:  'stop'
 	},
 	init: function(){
 		players.at(0).setCurrent(false);
@@ -67,23 +76,47 @@ $(function(){
 		players.at(1).set({time_remaining: 1200});
 	},
 	switchPlayer: function(){
+	    
+	    
+	    $(this.el).find('#play-pause i').removeClass('icon-play');
+	    $(this.el).find('#play-pause i').addClass('icon-pause');
 	    var currentPlayer = players.getCurrent(true);
 	    if(currentPlayer.length > 0){
 		var notCurrentPlayer = players.getCurrent(false);
 		currentPlayer[0].setCurrent(false);
-		notCurrentPlayer[0].setCurrent(true);
+		notCurrentPlayer[0].setCurrent(true);		
 	    }else{
 		players.at(0).setCurrent(true);
 	    }
 	},
-	play: function(){
-	    
+	playOrPause: function(){
+	    if(players.hasPlaying()){
+		this.pause();
+	    }else{
+		this.play();
+	    }
 	},
 	stop: function(){
+	    //TODO : create view PlayPause doit gérér l'état'
+	    $(this.el).find('#play-pause i').removeClass('icon-pause');
+	    $(this.el).find('#play-pause i').addClass('icon-play');
 	    this.init();
 	},
 	pause: function(){
-	    
+	    $(this.el).find('#play-pause i').removeClass('icon-pause');
+	    $(this.el).find('#play-pause i').addClass('icon-play');
+	    players.at(0).stop();
+	    players.at(1).stop();
+	},
+	play: function(){
+	    $(this.el).find('#play-pause i').removeClass('icon-play');
+	    $(this.el).find('#play-pause i').addClass('icon-pause');
+	    var currentPlayer = players.getCurrent(true);
+	    if(currentPlayer.length === 0){
+
+	    }else{
+		currentPlayer[0].play();
+	    }
 	},
 	params: function(){
 	    //window.location.href = '#params';
@@ -118,12 +151,16 @@ $(function(){
      */
     ParamView = Backbone.View.extend({
 	events: {
-		'change input' : 'changeValue'
+		//'change input' : 'changeValue'
+		//TODO onclose
 	},
         initialize : function() {
             this.template = _.template($('#param-template').html());
 	    this.listenTo(this.model, 'change', this.render);
 	    this.render();
+	    /*$('#myModal').on('hidden', function () {
+		this.changeValue();
+	      })*/
         },
 
         render : function() {
@@ -153,7 +190,7 @@ $(function(){
         render : function() {
             var renderedContent = this.template(this.model.toJSON());
             $(this.el).html(renderedContent);
-	    if( this.model.get('current') ){
+	    if( this.model.isPlaying() ){
 		$(this.el).addClass('current');
 	    }else{
 		$(this.el).removeClass('current');
@@ -175,6 +212,12 @@ $(function(){
 		    return !player.get('current');	
 		}
 	    })
+	},
+	hasPlaying: function(){
+	    var playerPlaying = this.find(function(player){
+		return player.isPlaying();	
+	    });
+	    return playerPlaying;
 	}
     });
      
